6d494e81b7696f62e187d096d5d9df4fb5f95ba8
jeffwalton <jeff.walton.ny@gmail.com>
2013-09-22 11:49:24 -0400

    commit 004

--- README.md
+++ README.md
@@ -5,5 +5,10 @@ A simple single-photo resection program written in python.
 
 Solution technique uses a performance function based on the collinearity equation that is minimized with scipy.optimize.minimize().
 
-Two input files are needed: camera calibration and control points.
+Two input files are needed: camera calibration and control points. Sample data is from: 
+    Introduction to Modern Photogrammetry by Mikhail, Bethel, McGlone
+    John Wiley & Sons, Inc. 2001
+
+Usage: python resection.py cam.inp resect.inp
+
 
--- resection.py
+++ resection.py
@@ -1,8 +1,35 @@
+# Author:  Jeffrey T. Walton, Paul Smith's College, New York
+#
+#   Single-photo resection - calculates the camera orientation and location
+#       given camera calibration parameters, control point photo and world
+#        coordinates and initial guesses for camera exterior orientation.
+#
+#   based on MATLAB code from:
+#   Introduction to Modern Photogrammetry by Mikhail, Bethel, McGlone
+#   John Wiley & Sons, Inc. 2001
+
+import sys
 import numpy as np
 from scipy.optimize import minimize
 
 
-def collinearityEqnResidual(iop,eop,x,y,X,Y,Z):
+def collinearity_eqn_residual(iop,eop,x,y,X,Y,Z):
+    """
+    Usage:
+        collinearity_eqn_residual(iop,eop,x,y,X,Y,Z)
+
+    Inputs:
+        iop = dict of interior orientation parameters: x0, y0, f
+        eop = dict of exterior orientation parameters: omega, phi, kappa, XL, YL, ZL
+        x = array of x photo coordinates of control points
+        y = array of y photo coordinates of control points
+        X = array of X world coordinates of control points
+        Y = array of Y world coordinates of control points
+        Z = array of Z world coordinates of control points
+
+    Returns:
+        residuals in x and y collinearity equations for a single point as a tuple
+    """
     from math import sin, cos
     x0 = iop['x0']
     y0 = iop['y0']
@@ -23,38 +50,44 @@ def collinearityEqnResidual(iop,eop,x,y,X,Y,Z):
     M = Mkp * Mph * Mom
 
     uvw = M * np.matrix([[X-XL], [Y-YL], [Z-ZL]])
-    #print uvw
 
-    #res = np.zeros((2,1))
-    #res[0,0] = x - x0 + focallength * uvw[0] / uvw[2]
-    #res[1,0] = y - y0 + focallength * uvw[1] / uvw[2]
     resx = x - x0 + focallength * uvw[0,0] / uvw[2,0]
     resy = y - y0 + focallength * uvw[1,0] / uvw[2,0]
 
     return resx, resy
 
 
+class CollinearityData:
+    """
+    class to store data for the collinearity equations
+    """
+    def __init__(self, camera_file, point_file):
+        """
+        initilizes data for collinearity equations
 
+        reads camera parameters from camera_file
+        reads control point data from point_file
+        """
 
-class collinearityData:
-    def __init__(self):
-
-        f = open('cam.inp','r')
+        f = open(camera_file,'r')
         dat = np.loadtxt(f,float)
         f.close
 
         self.eop = {}
 
+        # data from lines 1-3 of the camera_file
         self.eop['omega'] = dat[0]
         self.eop['phi'] = dat[1]
         self.eop['kappa'] = dat[2]
 
+        # data from lines 4-6 of the camera_file
         self.eop['XL'] = dat[3]
         self.eop['YL'] = dat[4]
         self.eop['ZL'] = dat[5]
 
         self.iop = {}
 
+        # data from lines 7-9 of the camera_file
         self.iop['x0'] = dat[6]
         self.iop['y0'] = dat[7]
         self.iop['f'] = dat[8]
@@ -66,15 +99,17 @@ class collinearityData:
         Y = []
         Z = []
 
-        f = open('resect.inp','r')
+        f = open(point_file,'r')
         for line in f:
             l = line.split()
+            # each line has 6 values: label, x, y, X, Y, Z (whitespace delimited)
             self.label.append(l[0])
             x.append(float(l[1]))
             y.append(float(l[2]))
             X.append(float(l[3]))
             Y.append(float(l[4]))
             Z.append(float(l[5]))
+        f.close
 
         self.x = np.array(x)
         self.y = np.array(y)
@@ -84,7 +119,19 @@ class collinearityData:
 
 
 
-def coll_func(indepVars):
+def coll_func(indep_vars):
+    """
+    collinearity function calculates a sum of the squared residuals of the
+        collinearity equations for all of the control points
+    This function is passed to scipy.optimize.minimize()
+
+    Inputs:
+        indep_vars (passed) are the exterior orientation parameters of the camera
+        data (global) camera interior calibration data, photo points, control points
+
+    Returns:
+        sum of squared residuals of collinearity eqns
+    """
     global data
     iop = data.iop
     #eop = data.eop
@@ -96,42 +143,53 @@ def coll_func(indepVars):
     Z = data.Z
 
     eop = {}
-    eop['omega'] = indepVars[0]
-    eop['phi'] = indepVars[1]
-    eop['kappa'] = indepVars[2]
-    eop['XL'] = indepVars[3]
-    eop['YL'] = indepVars[4]
-    eop['ZL'] = indepVars[5]
+    eop['omega'] = indep_vars[0]
+    eop['phi'] = indep_vars[1]
+    eop['kappa'] = indep_vars[2]
+    eop['XL'] = indep_vars[3]
+    eop['YL'] = indep_vars[4]
+    eop['ZL'] = indep_vars[5]
 
     i = 0
     F = 0.0
     for l in label:
 
-        F1, F2 = collinearityEqnResidual(iop,eop,x[i],y[i],X[i],Y[i],Z[i])
+        F1, F2 = collinearity_eqn_residual(iop,eop,x[i],y[i],X[i],Y[i],Z[i])
         F += F1**2 + F2**2
         i += 1
 
     return F
 
 
-data = collinearityData()
+if len(sys.argv) > 1:
+    camera_file = sys.argv[1]
+else:
+    camera_file = 'cam.inp'
+if len(sys.argv) > 2:
+    point_file = sys.argv[2]
+else:
+    point_file = 'resect.inp'
+
+data = CollinearityData(camera_file, point_file)
+
 x0 = np.zeros(6)
+# initilaize guesses for eop as read from file
 eop = data.eop
-#print eop
 x0[0] = eop['omega']
 x0[1] = eop['phi']
 x0[2] = eop['kappa']
 x0[3] = eop['XL']
 x0[4] = eop['YL']
 x0[5] = eop['ZL']
-#print x0
-#print data.iop
-#print data.x
-#print data.y
-#print data.X
-#print data.Y
-#print data.Z
 
-#res = minimize(coll_func, x0, method='BFGS', options={'disp': True})
-res = minimize(coll_func, x0, options={'disp': True})
-print res.x
+res = minimize(coll_func, x0, method='BFGS', options={'disp': True})
+#res = minimize(coll_func, x0, options={'disp': True})
+
+#print res.x
+print 'Solution:'
+print 'omega, ', res.x[0]
+print 'phi, ', res.x[1]
+print 'kappa, ', res.x[2]
+print 'XL, ', res.x[3]
+print 'YL, ', res.x[4]
+print 'ZL, ', res.x[5]
